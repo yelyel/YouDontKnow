6d1c811fb619e011ba04ec08eaec90ab5e053216
Yel <yel@film.ch>
2017-06-05 16:57:59 +0200

    intro fertig und Ordnerstruktur für kapitel angepasst

--- rithm/intro/rithm_Func.js
+++ rithm/intro/rithm_Func.js
@@ -282,18 +282,171 @@ console.log(instructor); // this will NOT throw an error!
 instructor = "Elie";
 
 
-// sayHi("Matt"); // "Hello Matt"
-//
-// function sayHi(name){
-//     return console.log("Hello " + name);
-// }
+sayHi("Matt"); // "Hello Matt"
 
-sayHi() // ?
-
-sayHi() // ?
-
-var sayHi = function(){
-    return 'Hi!';
+function sayHi(name){
+    return console.log("Hello " + name);
 }
 
+// sayHi() // ?
+//
+// sayHi() // ?
+//
+// var sayHi = function(){
+//     return 'Hi!';
+// }
+
 console.log(sayHi()); // ?
+
+console.log("   ");
+console.log("=========     EXERCISES     ========");
+console.log("   ");
+
+function difference(a, b) {
+  return console.log(a - b);
+}
+
+function product(a, b) {
+  return console.log(a * b);
+}
+difference(2,2); // 0
+difference(0,2); // -2
+
+product(2,2); // 4
+product(0,2); // 0
+
+function printDay(a) {
+  var weekday = [];
+  weekday[0] =  "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+  if (1 <= a <= 7) {
+
+    return console.log(weekday[a - 1]);
+  }
+}
+
+printDay(4); // "Wednesday"
+printDay(41); // undefined
+
+function lastElement(arr) {
+  return console.log(arr[arr.length - 1]);
+}
+
+lastElement([1,2,3,4]); // 4
+lastElement([]); // undefined
+
+function numberCompare(f, g) {
+  if ( f == g) {
+    return console.log(" numbers equal ");
+  } else if (f > g) {
+    return console.log(" first greater ");
+  }
+  return console.log(" first smaller ");
+}
+
+numberCompare(1,1); // "Numbers are equal"
+numberCompare(2,1); // "First is greater"
+numberCompare(1,2); // "Second is greater"
+
+function singleLetterCount(str, letter) {
+  var count = 0;
+  for (var i = 0 ; i < str.length; i++) {
+    if (str[i].toUpperCase() === letter.toUpperCase()) {
+      count++;
+    }
+  }
+  return console.log(count);
+}
+
+
+singleLetterCount('amazing','A'); // 2
+singleLetterCount('Rithm School','o'); // 2
+
+
+function multipleLetterCount(str) {
+  var arr = [];
+  for (var i = 0 ; i < str.length; i++) {
+    if (str[i] in arr) {
+      arr[str[i]]++;
+    } else {
+      arr[str[i]] = 1;
+    }
+  } return console.log(arr);
+}
+
+// function multipleLetterCount(str){
+//   var finalObj = {};
+//   for(var i =0; i< str.length; i++){
+//     if (!(str[i] in finalObj)){
+//       finalObj[str[i]] = 1;
+//     } else {
+//       finalObj[str[i]]++;
+//     }
+//   }
+//   return console.log(finalObj);
+// }
+
+multipleLetterCount("hello"); // {h:1, e: 1, l: 2, o:1}
+multipleLetterCount("person"); // {p:1, e: 1, r: 1, s:1, o:1, n:1}
+
+
+function arrayManipulation(arr, comm, place, num) {
+  if (comm == "remove") {
+    if (place == "end") {
+      return console.log(arr.pop());
+    }
+    return console.log(arr.shift());
+
+  } else if (comm == "add") {
+    if (place == "end") {
+      arr.push(num);
+      return console.log(arr);
+    }
+    arr.unshift(num);
+    return console.log(arr);
+  }
+}
+
+
+arrayManipulation([1,2,3], "remove", "end"); // 3
+arrayManipulation([1,2,3], "remove", "beginning"); // 1
+arrayManipulation([1,2,3], "add", "beginning", 20); // [20,1,2,3]
+arrayManipulation([1,2,3], "add", "end", 30); // [1,2,3,30]
+
+function isPalindrome(str) {
+    var revstr = str.split("").reverse().join("");
+    return console.log(revstr == str);
+}
+
+isPalindrome('testing'); // false
+isPalindrome('tacocat'); // true
+isPalindrome('hannah'); // true
+isPalindrome('robert'); // false
+
+function stonePaperSissor(val) {
+  var val = val.toLowerCase();
+  var choices = ["scissor", "paper", "stone"];
+  var choice = choices[Math.floor(Math.random()*(2 - 0 + 1))];
+  //var choice = "scissor";
+  console.log("choice : " + choice);
+  if ( val == choice) {
+    return console.log(" NO Winner !  ");
+  }
+  if (val === "paper" && choice === "scissor") {
+    return console.log("you LOOSE !!  ");
+  }
+  if (val === "stone" && choice === "paper") {
+    return console.log("you LOOSE !!  ");
+  }
+  if (val === "scissor" && choice === "stone") {
+    return console.log("you LOOSE !!  ");
+  } else {
+    return console.log("you WIIINNNNN !!  ");
+  }
+}
+stonePaperSissor("scissor");
