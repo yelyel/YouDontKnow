51f73d3f29fa48f9e66b22024b55bcb176a36267
Yel <yel@film.ch>
2017-06-09 23:16:30 +0200

    einige  closure tests zu closure.js hinzugefügt

--- rithm/intermediate_I/closures.js
+++ rithm/intermediate_I/closures.js
@@ -13,11 +13,34 @@ console.log(out(10));
 // out = outer(9)(7);
 //
 // console.log(out);
-
-
+//var out = outer(11);
+//out(12)
+console.log(out(15));  //24
 var laterAdd = outer(10);
 
 // we can now call that inner function
 
 
 console.log(laterAdd(15));
+
+function newAge(){
+   var age = 21;
+    return function growUp(){
+        return ++age;
+    }
+}
+var agePlus = newAge();
+console.log(agePlus());
+console.log(agePlus());
+console.log(agePlus());
+console.log(agePlus);
+console.log(agePlus());
+console.log(agePlus.age);
+
+
+function defineAge(){
+    var age = 28;
+    return function growUp(){
+        return ++age;
+    }
+}
