add16f789cf76149dc16f3b14f0d7bbcae8d84c5
yelyel <yel@film.ch>
2017-06-30 15:40:02 +0200

    tictactoe fertig ohne Zusätze

--- rithm/intermediate_I/tests.js
+++ rithm/intermediate_I/tests.js
@@ -8,3 +8,17 @@ function numberFact(num, fn){
 }
 
 console.log(numberFact(49, isOdd));
+var player = {score: 1, name: "Yel"};
+
+console.log("1 : " + player.name + " score : " + player.score);
+
+//var newPlayer = {...player, score: 2};
+var newPlayer = Object.assign({}, player, {score: 2});
+console.log("2 : " + player.name + " score : " + player.score);
+console.log("2 : " + newPlayer.name + " score : " + newPlayer.score);
+const lines = [
+  [1, 2, 3],
+  [4, 5, 6]
+];
+const [a, b, c] = lines[1];
+console.log("a : " + a);
