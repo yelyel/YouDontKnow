b713cef58abd4af9096555f195a3fadebe831703
yelyel <yel@film.ch>
2017-07-12 15:00:30 +0200

    an introduction und thinking weitergearbeitet

--- reactTut/tictactoe/src/index.js
+++ reactTut/tictactoe/src/index.js
@@ -1,54 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatName(user) {
-  return user.firstName + " " + user.lastName;
-}
+// function formatName(user) {
+//   return user.firstName + " " + user.lastName;
+// }
+//
+// var user = {
+//   firstName: "Hans",
+//   lastName: "Frey"
+// }
+//
+// const element = (
+//   <div>
+//   <h1>
+//     Hello,??? {formatName(user)}
+//   </h1>
+//   </div>
+// );
+// class Element2 extends React.Component {
+//   render() {
+//     return (
+//       <h1>Helloooooooo ?????? {formatName(user)} </h1>);
+//   }
+// }
+//
+// function showGreeting(user) {
+//   if(user) {
+//     return (
+//       <div>
+//       <Element2 />
+//       <h2> Hello!!, {formatName(user)} </h2>
+//       </div>
+//     );
+//   }
+//   return (
+//     <div>
+//     <h2> Hello Stranger </h2>
+//     </div>
+//   );
+// }
+//
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello,??? {formatName(user)} </h1>
+//       <h2> it si now {new Date().toLocaleTimeString()}. </h2>
+//     </div>
+//   );
+// }
+//
+// function showAll(user) {
+//   return (
+//     element
+//   );
+// }
+// // wieso wird element hier nicht auch gerendert ??
+// ReactDOM.render(
+//   //showAll(),
+//   //showGreeting(user),
+//   element,
+//   document.getElementById('root')
+// );
+//
+// setInterval(tick, 1000);
 
-var user = {
-  firstName: "Hans",
-  lastName: "Frey"
-}
-
-const element = (
-  <div>
-  <h1>
-    Hello,??? {formatName(user)}
-  </h1>
-  </div>
-);
-class Element2 {
-  render() {
-    return "Hello,???" + formatName(user);
-  }
-}
-
-function showGreeting(user) {
-  if(user) {
-    return (
-      <div>
-      <Element2 />
-      <h2> Hello!!, {formatName(user)} </h2>
-      </div>
-    );
-  }
-  return (
+function tick() {
+  const element = (
     <div>
-    <h2> Hello Stranger </h2>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
-}
-
-
-function showAll(user) {
-  return (
-    element
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
   );
 }
-// wieso wird element hier nicht auch gerendert ??
-ReactDOM.render(
-  //showAll(),
-  showGreeting(user),
-  //element,
-  document.getElementById('root')
-);
+
+setInterval(tick, 1000);
---
+++ reactTut/tictactoe/src/introJSX_index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+var user = {
+  firstName: "Hans",
+  lastName: "Frey"
+}
+
+const element = (
+  <div>
+  <h1>
+    Hello,??? {formatName(user)}
+  </h1>
+  </div>
+);
+class Element2 extends React.Component {
+  render() {
+    return (
+      <h1>Helloooooooo, {formatName(user)} </h1>);
+  }
+}
+
+function showGreeting(user) {
+  if(user) {
+    return (
+      <div>
+      <Element2 />
+      <h2> Hello!!, {formatName(user)} </h2>
+      </div>
+    );
+  }
+  return (
+    <div>
+    <h2> Hello Stranger </h2>
+    </div>
+  );
+}
+
+
+function showAll(user) {
+  return (
+    element
+  );
+}
+// wieso wird element hier nicht auch gerendert ??
+ReactDOM.render(
+  //showAll(),
+  showGreeting(user),
+  //element,
+  document.getElementById('root')
+);
