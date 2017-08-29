0d81539e6ce9cf6e5fe6650867c047b960664d93
yelyel <yel@film.ch>
2017-08-29 13:12:19 +0200

    forms erfolgreich beendet

--- reactTut/projectmanager/src/index.js
+++ reactTut/projectmanager/src/index.js
@@ -43,60 +43,122 @@ import ReactDOM from 'react-dom';
 //     );
 //   }
 // }
+//
+// ReactDOM.render(
+//   <NameForm  />,
+//   document.getElementById('root')
+// );
 
-class SelectForm extends React.Component {
+//=================================================================================
+//  Select Form
+
+
+// class SelectForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {value: 'oranges'};
+//
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//
+//   }
+//
+//   handleChange(event) {
+//     this.setState({value: event.target.value});
+//     console.log(event.target.value);
+//   }
+//
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     alert('Neues Gemüse eingegeben: ' + this.state.value);
+//   }
+//
+//   render() {
+//     return (
+//       <form onSubmit = {this.handleSubmit}>
+//         <label>
+//           Welches Schweindl hàttens gern ?:
+//           <br />
+//             <select value = {this.state.value} onChange = {this.handleChange}>
+//             <option value = 'oranges'>Orangen</option>
+//             <option value = 'manda'>Manda</option>
+//             <option value = 'apples'>Apfles</option>
+//             <option value = 'käse'>Käse</option>
+//           </select>
+//         </label>
+//         <input type = 'submit' value = 'SUBMIT' />
+//       </form>
+//     );
+//   }
+// }
+//
+// ReactDOM.render(
+//   <SelectForm  />,
+//   document.getElementById('root')
+// );
+
+
+// =============================================================================
+// Multiple input Forms
+
+
+class Reservation extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'coconut'};
-
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-    console.log(event.target.value);
   }
 
   handleSubmit(event) {
-    event.preventDefault();
-    alert('Neues Gemüse eingegeben: ' + this.state.value);
+      event.preventDefault();
+      //let isGoing = this.state.isGoing;
+      let message = this.state.isGoing ? this.state.numberOfGuests +  " are coming !" : "nobody";
+      alert(message);
+    }
+  handleInputChange(event)
+  {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
   }
-
   render() {
-    return (
-      //<form onSubmit = {this.handleSubmit}>
-      //   <label>
-      //     Welches Schweindl hättens gern ?:
-      //     <br />
-      //     <select value = {this.state.value} onChange = {this.handleChange} />
-      //       <option value = 'oranges'>Orangen</option>
-      //       <option value = 'manda'>Manda</option>
-      //       <option value = 'apples'>Apfles</option>
-      //       <option value = 'käse'>Käse</option>
-      //     </select>
-      //   </label>
-      //   <input type = 'submit' value = 'SUBMIT' />
-      // </form>
-      <form onSubmit={this.handleSubmit}>
-      {/*<form onSubmit={this.handleSubmit}>*/}
-        <label>
-          Pick your favorite La Croix flavor:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">Grapefruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-            <option value="mango">Mango</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-        </form>
+    return(
+      <form onSubmit = {this.handleSubmit}>
+      <label>
+      Is going:
+        <input
+          type = 'checkbox'
+          name = 'isGoing'
+          checked = {this.state.isGoing}
+          onChange = {this.handleInputChange}
+        />
+      </label>
+      <br />
+      <label>
+      Anzahl Gäste:
+        <input
+          type = 'number'
+          name = 'numberOfGuests'
+          value = {this.state.numberOfGuests}
+          onChange = {this.handleInputChange}
+        />
+      </label>
+        <input type = 'submit' value ='Send !!' />
+      </form>
+
     );
+
   }
 }
 
 ReactDOM.render(
-  <SelectForm  />,
+  <Reservation />,
   document.getElementById('root')
 );
